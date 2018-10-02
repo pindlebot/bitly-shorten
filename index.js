d631d54b7274c888673bc0cabff5bfd72f87a613
Ben <menubar@protonmail.com>
2018-10-02 01:06:12 -0400

    new

---
+++ index.js
@@ -0,0 +1,20 @@
+const safeEncodeURIComponent = string => {
+  let decoded = decodeURIComponent(string)
+  while (string !== decoded) {
+    string = decoded
+    decoded = decodeURIComponent(string)
+  }
+  return encodeURIComponent(decoded)
+}
+
+module.exports = (url, { apiKey } = {}) => {
+  const bitlyApiKey = apiKey || process.env.BITLY_API_KEY
+  let longUrl = safeEncodeURIComponent(url)
+  let url = `https://api-ssl.bitly.com/v3/shorten?access_token=${bitlyApiKey}&longUrl=${longUrl}`
+  return fetch(url, {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  .then(resp => resp.json())
+}
